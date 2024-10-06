271bc3f8830832c322cf5266c44ea5abb2d269a2
DrKillshot <bruno.mota@kuehne-nagel.com>
2024-10-06 22:19:13 +0100

    Added documentation deployment action

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/@unnullable/unwrap',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
