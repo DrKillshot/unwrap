6a5046c09e43feba9d6cff0adf5adf0061915749
DrKillshot <bruno.mota@kuehne-nagel.com>
2024-10-22 23:56:47 +0100

    Started test for branded types

--- src/branded/types.ts
+++ src/branded/types.ts
@@ -1,20 +1,21 @@
 declare const __brand: unique symbol
 type Brand<B> = { readonly [__brand]: B 
 }
+type aBrand = {[__brand]: string}
 type Predicate<T> = (input: T) => boolean
 type StringFunction<T> = (input: T) => string
-type BrandConstructor<T extends {[__brand]: string}> = (param: Omit<T, symbol>) => T
+type BrandConstructor<T extends aBrand> = (param: Omit<T, symbol>) => T
 
 export namespace Brand {
-    export type Type<B, T> = T & Brand<B>
+    export type type<B, T> = T & Brand<B>
 
-    export function create<T extends Type<any, any>>(): (param: Omit<T, symbol>) => T {
+    export function create<T extends type<any, any>>(): (param: Omit<T, symbol>) => T {
         return (param) => {
             return param as T
         }
     }
 
-    export function define<T extends {[__brand]: string}>(
+    export function define<T extends aBrand>(
         predicate: Predicate<T>,
         error: StringFunction<Omit<T, symbol>> | string
     ): BrandConstructor<T> {
@@ -27,13 +28,18 @@ export namespace Brand {
         }
     }
 
-    export function combine<T extends string, R>(...constructors: BrandConstructor<Type<any, R>>[]): (param: R) => Type<T, R> {
+    export function combine<T extends string, R>(...constructors: BrandConstructor<type<any, R>>[]): (param: R) => type<T, R> {
         return (param) => {
             for(const constructor of constructors) {
                 constructor(param as any)
             }
     
-            return param as Type<T, R>
+            return param as type<T, R>
         }
     }
 }
+
+/* TODO: Method to create the oposite constructor. Example: 
+const Integer = Brand.define<Brand.type<"Integer", number>>(n => Number.isInteger(n), n => `Expected an integer. Instead got ${n}`)
+const NonInteger = OpositeMehtod(Integer)
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { Brand } from "./branded/types";
 import { Either } from "./containers/either";
 import { IO } from "./containers/io";
 import { Maybe } from "./containers/maybe";
 import { Data, Empty } from "./pattern-matching/data";
 
 // TODO: Create issue to seperate path to resources, e.g import {Maybe} from "@unnulable/unwrap/containers"
-export { Data, Either, Empty, IO, Maybe };
+export { Brand, Data, Either, Empty, IO, Maybe };
 
