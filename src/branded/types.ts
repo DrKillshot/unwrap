51879c260bf9c8ba85677f58a5ce34f2ec2028a9
DrKillshot <bruno.mota@kuehne-nagel.com>
2024-10-22 22:07:24 +0100

    Added combine operator and moved brand method into their own namespace

--- src/branded/types.ts
+++ src/branded/types.ts
@@ -1,30 +1,39 @@
 declare const __brand: unique symbol
 type Brand<B> = { readonly [__brand]: B 
 }
-
-export type Type<B, T> = T & Brand<B>
-
 type Predicate<T> = (input: T) => boolean
 type StringFunction<T> = (input: T) => string
+type BrandConstructor<T extends {[__brand]: string}> = (param: Omit<T, symbol>) => T
 
-export function Create<T extends Type<any, any>>(): (param: Omit<T, symbol>) => T {
-    return (param) => {
-        return param as T
-    }
-}
+export namespace Brand {
+    export type Type<B, T> = T & Brand<B>
 
-export function Define<T extends {[__brand]: string}>(
-    predicate: Predicate<Omit<T, symbol>>,
-    error: StringFunction<Omit<T, symbol>> | string
-) {
-    return (param: Omit<T, symbol>) => {
-        if(!predicate(param)) {
-            throw new Error(typeof error === "function" ? error(param) : error)
+    export function create<T extends Type<any, any>>(): (param: Omit<T, symbol>) => T {
+        return (param) => {
+            return param as T
         }
-        
-        return param as T
+    }
+
+    export function define<T extends {[__brand]: string}>(
+        predicate: Predicate<T>,
+        error: StringFunction<Omit<T, symbol>> | string
+    ): BrandConstructor<T> {
+        return (param: Omit<T, symbol>) => {
+            if(!predicate(param as any)) {
+                throw new Error(typeof error === "function" ? error(param) : error)
+            }
+            
+            return param as T
+        }
+    }
+
+    export function combine<T extends string, R>(...constructors: BrandConstructor<Type<any, R>>[]): (param: R) => Type<T, R> {
+        return (param) => {
+            for(const constructor of constructors) {
+                constructor(param as any)
+            }
+    
+            return param as Type<T, R>
+        }
     }
 }
-
-// TODO: Implement combine function
-export function Combine() {}
